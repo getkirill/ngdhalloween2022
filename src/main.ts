ba5ee06531b90404313218d062479caee7c5b13f
Kraskaska <k.krasilnikov.2008@gmail.com>
2022-10-27 17:50:10 +0300

    Game can at least render cyan color.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
-import { Game } from 'phaser'
-import './style.scss'
+import "./style.scss";
+import "./game";
+import { NGDHalloween2022Game } from "./game";
 
-new Game({canvas: document.querySelector("#mount-point") as HTMLCanvasElement})
+// Because we are not visited frequently chrome doesn't allow autoplay of music. So, we trick it by making a start button that forces user interaction which in turn allows us to play sounds.
+(document.querySelector("#start-button") as HTMLButtonElement).addEventListener(
+  "click",
+  () => {
+    document.querySelector("#start-button")?.classList.add("hidden");
+    document
+      .querySelector(".wrapper:has(#mount-point)")
+      ?.classList.remove("hidden");
+    new NGDHalloween2022Game(
+      document.querySelector("#mount-point") as HTMLCanvasElement
+    );
+  }
+);
