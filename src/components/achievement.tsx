cbf1e8b0a20802e3a8adb7641bb3953920ae1608
Philainel <toropovmat@gmail.com>
2022-10-30 22:11:01 +0300

    Add Electron

---
+++ src/components/achievement.tsx
@@ -0,0 +1,13 @@
+import "../styles/components/achievement.scss"
+
+export default function Achievment() {
+	return (
+		<div className="achievement">
+			<div className="icon"></div>
+			<div className="text">
+				<h4 className="title">Header</h4>
+				<span className="description">desk</span>
+			</div>
+		</div>
+	)
+}
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import electron from "vite-electron-plugin"
+import { customStart } from "vite-electron-plugin/plugin"
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    electron({
+      include: [
+        'electron',
+        'preload',
+      ],
+      // plugins: [
+      //   customStart()
+      // ]
+    })
+  ]
 })
